0beae20204c711cb66a69f46f9bbb9b598a2202f
synelnoor <synelnoor@gmail.com>
2022-07-23 13:31:43 +0700

    added sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -27,8 +27,7 @@
         <a class="normal-case m-3 text-xl text-gray-400 hover:text-red-400" href="{{ route('welcome') }}" >synGEN</a>
       </div>
       @endguest
-      
-      
+
       <div class="flex-none">
         @guest
           @if (Route::has('login')) 
@@ -54,13 +53,13 @@
             </label>
             <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-gray-400">
               <li>
-                <a class="justify-between">
+                <a class="justify-between hover:text-red-400">
                   Profile
                 </a>
               </li>
-              <li><a>Settings</a></li>
+              <li><a class="hover:text-red-400">Settings</a></li>
               <li>
-                <a href="{{ route('logout') }}"
+                <a class="hover:text-red-400" href="{{ route('logout') }}"
                     onclick="event.preventDefault();
                                   document.getElementById('logout-form').submit();">
                     {{ __('Logout') }}
@@ -83,39 +82,7 @@
       </div> 
       <div class="drawer-side">
         <label for="my-drawer" class="drawer-overlay"></label>
-        <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-          <!-- Sidebar content here -->
-          <li class="menu-title">
-            <span>Category</span>
-          </li>
-          <li>
-            <a>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-            Sidebar Item 1
-            </a>
-          </li>
-          <li>
-            <a>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-            Sidebar Item 2
-            </a>
-          </li>
-          <li>
-            <a>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
-            Sidebar Item 3
-            </a>
-          </li>
-          <li class="menu-title">
-            <span>Category 2</span>
-          </li>
-          <li>
-            <a>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-            Sidebar Item 4
-            </a>
-          </li>
-        </ul>
+        @include('includes.sidebar')
       </div>
     </div>
   </div>
